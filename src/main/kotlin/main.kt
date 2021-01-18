a2c794bb070bee6b98c0679db59aba32f01e0069
TurcnaninovUV <pro-h92@yandex.ru>
2021-01-18 18:19:56 +0300

    Исправил замечания

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -5,9 +5,11 @@ fun main() {
 }
 
 fun costing(transferAmount: Int) {
-    var amount = transferAmount * 99.25 / 100
-    if (transferAmount - amount < 3500)
-        print("Сумма перевода с комиссией будет составлять - ${transferAmount - 3500} в копейках" +
-                " (${(transferAmount - 3500) / 100} в рублях)")
-    else print("Сумма перевода с комиссией будет составлять - $amount в копейках ( ${amount / 100} в рублях)")
+    var amount = (transferAmount * 0.95).toInt()
+    if (transferAmount - amount < 3500) amount = transferAmount - 3500
+    print(
+        "Сумма перевода с комиссией будет составлять - $amount в копейках" +
+                " (${amount / 100} в рублях)"
+    )
+
 }
